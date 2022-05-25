0b281b79a97e1b260523c776871eebe9f7626b11
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-05-25 18:01:08 +0000

    Car working

--- car.js
+++ car.js
@@ -2,19 +2,33 @@ class Car{
     constructor(x,y,width,height){
         this.x=x;
         this.y=y
-        this.widht=width;
+        this.width=width;
         this.height=height;
+
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
 
         this.controls=new Controls();
     }
 
     update(){
         if(this.controls.forward){
-            this.y-=2; 
+            this.speed+=this.acceleration; 
         }
         if(this.controls.reverse){
-            this.y+=2;
+            this.speed-=this.acceleration;
         }
+
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+
+        this.y-=this.speed;
     }
 
 
