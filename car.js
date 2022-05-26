4a6fc507810f6b840d58eacb62ab0620f05011d3
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-05-26 16:29:08 +0000

    Car mechanics

--- car.js
+++ car.js
@@ -9,11 +9,16 @@ class Car{
         this.acceleration=0.2;
         this.maxSpeed=3;
         this.friction=0.05;
+        this.angle=0;
 
         this.controls=new Controls();
     }
 
     update(){
+        this.#move();
+    }
+
+    #move(){
         if(this.controls.forward){
             this.speed+=this.acceleration; 
         }
@@ -28,18 +33,46 @@ class Car{
             this.speed=-this.maxSpeed/2;
         }
 
-        this.y-=this.speed;
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+        
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
 
 
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
