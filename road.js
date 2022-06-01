96dc6f3d963dbd2c5bb3d2d7515021eceaeeaa2b
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-06-01 13:56:05 +0000

    Estrada corrigida

--- road.js
+++ road.js
@@ -13,8 +13,8 @@ class Road{
 
         const topLeft={x:this.left,y:this.top};
         const topRight={x:this.right,y:this.top};
-        const bottomLeft={x:this.left,y:this.top};
-        const bottomRight={x:this.right,y:this.top};
+        const bottomLeft={x:this.left,y:this.bottom};
+        const bottomRight={x:this.right,y:this.bottom};
 
         this.borders=[
             [topLeft,bottomLeft],
