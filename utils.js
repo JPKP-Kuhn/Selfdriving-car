0407a806e740fa70d64221841d6e42b6889be1c8
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-06-02 11:19:40 +0000

    55:40 do vídeo

--- utils.js
+++ utils.js
@@ -1,3 +1,22 @@
 function lerp(A,B,t){
     return A+(B-A)*t;
 }
+function getIntersection(A,B,C,D){
+    const tTop=(D.x-C.x)*(A.y-C.y)-(D.y)
+    const uTop=(C.y-A.y)*(A.x-B.x)-(C.x)
+    const bottom=(D.y-C.y)*(B.x-A.x)-(D)
+
+    if(bottom!=0){
+        const t=tTop/bottom;
+        const u=uTop/bottom;
+        if(t>=0 && t<=1 && u>=0 && u<=1){
+            return {
+                x:lerp(A.x,B.x,t),
+                y:lerp(A.y,B.y,t),
+                offset:t
+            }
+        }
+    }
+
+    return null;
+}
