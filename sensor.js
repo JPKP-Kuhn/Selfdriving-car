d22de72c6dffd8ed725b205366035b099a92aaa9
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-06-13 14:26:56 +0000

    Sensor não funciona ainda

--- sensor.js
+++ sensor.js
@@ -1,28 +1,47 @@
 class Sensor{
-    this.car=car;
-    this.rayCount=3;
-    this.rayLength=100;
-    this.raySpread=Math.PI/4;
+    constructor(car){
+        this.car=car;
+        this.rayCount=3;
+        this.rayLength=100;
+        this.raySpread=Math.PI/4;
 
-    this.rays=[];
-}
+        this.rays=[];
+    }
 
-update(){
-    this.rays=[];
-    for(let i=0;i<this.rayCount;i++){
-        const rayAngle=lerp(
-            this.raySpread/2,
-            -this.raySpread/2,
-            i/(this.rayCount-1)
-        );
+    update(){
+        this.rays=[];
+        for(let i=0;i<this.rayCount;i++){
+            const rayAngle=lerp(
+                this.raySpread/2,
+                -this.raySpread/2,
+                i/(this.rayCount-1)
+            );
 
-        const star={x:this.car.x, y:this.car.y};
-        const end={
-            x:this.car.x-
-              Math.sin(rayAngle)*this.rayLength,
-            y:this.car.y-
-               Math.cos(rayAngle)*this.rayLength
-        };
-        this.rays.push{[start,end]};
+            const star={x:this.car.x, y:this.car.y};
+            const end={
+                x:this.car.x-
+                    Math.sin(rayAngle)*this.rayLength,
+                y:this.car.y-
+                    Math.cos(rayAngle)*this.rayLength
+            };
+        this.rays.push([start,end]);
+        }
+    }
+    draw(ctx){
+        for(let i=0;i<this.rayCount;i++){
+            ctx.beginPath();
+            ctx.lineWidth=2;
+            ctx.strokeStyle="yellow";
+            ctx.moveTo(
+                this.rays[i][0].x,
+                this.rays[i][0].y
+            );
+            ctx.lineTo(
+                this.rays[i][1].x,
+                this.rays[i][1].y
+            );
+            ctx.stroke();
+              
+        }
     }
 }
