3cd923c6e52c920cabb8ec6b298d8774b59b1a28
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-06-21 00:56:45 +0000

    1h 02 min

--- sensor.js
+++ sensor.js
@@ -1,14 +1,14 @@
 class Sensor{
     constructor(car){
         this.car=car;
-        this.rayCount=3;
-        this.rayLength=100;
-        this.raySpread=Math.PI/4;
+        this.rayCount=5;
+        this.rayLength=150;
+        this.raySpread=Math.PI/2;
 
         this.rays=[];
     }
 
-    update(){
+    update(roadBorders){
        this.#castRays();
     }
 
