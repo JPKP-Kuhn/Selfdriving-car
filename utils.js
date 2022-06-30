36724c0c9e35fa5befc1002251bcdc2675294072
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-06-30 16:26:41 +0000

    Another car

--- utils.js
+++ utils.js
@@ -28,7 +28,7 @@ function polysIntersect(poly1, poly2){
                 poly1[i],
                 poly1[(i+1)%poly1.length],
                 poly2[j],
-                poly2[(j+1)%poly2.lenth]   
+                poly2[(j+1)%poly2.length]   
             );
             if(touch){
                 return true;
