2f37f70d2cf196f1ab77ad78b4a6a6d1e21ef6bb
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-07-02 02:38:08 +0000

    Traffic simulating

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9);
 const car=new Car(road.getLaneCenter(1),100,30,50,"KEYS");
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY")
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
 ];
 
 animate();
 
 function animate(){
     for(let i=0;i<traffic.length;i++){
-        traffic[i].update(road.borders);
+        traffic[i].update(road.borders,[]);
     }
-    car.update(road.borders);
+    car.update(road.borders,traffic);
     
     canvas.height=window.innerHeight;
 
@@ -23,9 +23,9 @@ function animate(){
 
     road.draw(ctx);
     for(let i=0;i<traffic.length;i++){
-        traffic[i].draw(ctx)
+        traffic[i].draw(ctx,"red")
     }
-    car.draw(ctx);
+    car.draw(ctx,"blue");
 
     ctx.restore();
     requestAnimationFrame(animate);
