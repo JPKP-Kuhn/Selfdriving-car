4fa78f16d173c52a54858edc9d68c306e1442a96
João P. Kuhn <pedrojoaojpk@gmail.com>
2022-07-07 16:15:05 +0000

    first layer

--- network.js
+++ network.js
@@ -27,7 +27,22 @@ class Level{
 
     static feedForward(givenInputs,level){
         for(let i=0;i<level.inputs.length;i++){
-            level.inputs[i]=givenInput
+            level.inputs[i]=givenInput[i];
         }
+
+        for(let i=0;i<level.outputs.length;i++){
+            let sum=0
+            for(let j=0;j<level.inputs.length;j++){
+                sum+=level.inputs[j]*level.weights[j][i];
+            }
+
+            if(sum>level.biases[i]){
+                level.outputs[i]=1;
+            }else{
+                level.outputs[i]=0;
+            }
+        }
+
+        return level.outputs;
     }
 }
